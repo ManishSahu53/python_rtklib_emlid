0847e2299efb0126df1241c2a441aab0bcebab05
Manish <manishsahu53@Live.com>
2018-08-24 10:37:35 +0530

    code cleanup

--- process_rtk.py
+++ process_rtk.py
@@ -3,19 +3,28 @@ def check_dir(path):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+
 def booldialogbox(message):
-    result = tkMessageBox.askquestion("Provide you input",message, icon='warning')
+    result = tkMessageBox.askquestion(
+        "Provide you input", message, icon='warning')
     if result == 'yes':
         return 1
     else:
         return 0
 
-import os, sys, subprocess
-import pexif,shutil
+
+import os
+import sys
+import subprocess
+import pexif
+import shutil
 import shapefile
 import numpy as np
-import Tkinter, Tkconstants, tkFileDialog, tkMessageBox
-from Tkinter import * 
+import Tkinter
+import Tkconstants
+import tkFileDialog
+import tkMessageBox
+from Tkinter import *
 
 import zipfile
 from os.path import basename
@@ -24,14 +33,17 @@ import pandas as pd
 root = Tk()
 
 # Reading zip files
-base_zip  = tkFileDialog.askopenfilename(initialdir = "/",title = "Select Base ubx zip file",filetypes = (("zip files","*.zip"),("all files","*.*")));
-rover_zip = tkFileDialog.askopenfilenames(initialdir = "/",title = "Select Rover ubx files",filetypes = (("zip files","*.zip"),("all files","*.*")));
+base_zip = tkFileDialog.askopenfilename(
+    initialdir="/", title="Select Base ubx zip file", filetypes=(("zip files", "*.zip"), ("all files", "*.*")))
+rover_zip = tkFileDialog.askopenfilenames(
+    initialdir="/", title="Select Rover ubx files", filetypes=(("zip files", "*.zip"), ("all files", "*.*")))
 
 # Asking for Position of Base
 base_pos_prompt = booldialogbox("Do you want enter position of base?")
 
-if base_pos_prompt == 1: # Taking Base coordinates, then enter csv else continue
-    base_pos = tkFileDialog.askopenfilename(initialdir = "/",title = "Select Position csv files",filetypes = (("csv file","*.csv"),("all files","*.*")));
+if base_pos_prompt == 1:  # Taking Base coordinates, then enter csv else continue
+    base_pos = tkFileDialog.askopenfilename(
+        initialdir="/", title="Select Position csv files", filetypes=(("csv file", "*.csv"), ("all files", "*.*")))
 
     # Reading csv as pandas dataframe and then numpy array
     # Lat, Long, Ele
@@ -44,17 +56,18 @@ if base_pos_prompt == 1: # Taking Base coordinates, then enter csv else continue
     print(base_position)
 
 # Is it static mode or kinematic mode
-mode_prompt = booldialogbox("Process with Static mode? Yes- Static mode, No- Kinematic mode")
+mode_prompt = booldialogbox(
+    "Process with Static mode? Yes- Static mode, No- Kinematic mode")
 if mode_prompt == 1:
-    mode = str(3) # Static
+    mode = str(3)  # Static
 else:
-    mode = str(2) # Kinematic
+    mode = str(2)  # Kinematic
 
 
 # Setting executable path files
 exe_path = 'src/2.4.3'
-conv_file = os.path.join(exe_path,'CONVBIN.exe')
-post_file = os.path.join(exe_path,'rnx2rtkp.exe')
+conv_file = os.path.join(exe_path, 'CONVBIN.exe')
+post_file = os.path.join(exe_path, 'rnx2rtkp.exe')
 
 # Setting Base extraction location
 base_extract = os.path.splitext(base_zip)[0]
@@ -66,7 +79,8 @@ basezip.extractall(base_extract)
 basezip.close()
 
 # Runnning base converter
-run_base_converter = subprocess.Popen( [conv_file,  os.path.join(base_extract, basename(base_extract)[:-4] + ".ubx"), "-r", "ubx", "-v", "3.03"] )
+run_base_converter = subprocess.Popen([conv_file,  os.path.join(
+    base_extract, basename(base_extract)[:-4] + ".ubx"), "-r", "ubx", "-v", "3.03"])
 run_base_converter.wait()
 
 print(base_extract, base_extract[:-4] + ".ubx")
@@ -80,30 +94,36 @@ for rov in rover_zip:
     print(os.path.join(rover_extract, rover_extract[:-4] + ".ubx"))
 
     # Running rover converter
-    run_rover_converter = subprocess.Popen( [conv_file, os.path.join(rover_extract, basename(rover_extract)[:-4] + ".ubx"), "-r", "ubx", "-v", "3.03"] )
+    run_rover_converter = subprocess.Popen([conv_file, os.path.join(
+        rover_extract, basename(rover_extract)[:-4] + ".ubx"), "-r", "ubx", "-v", "3.03"])
     run_rover_converter.wait()
 
     # Running Post Processing
     # Rover Obs, Base Obs, nav, gnav, hnav, lnav, qnav, -c on -i on -h on
 
-    if base_pos_prompt == 0: # No position file is given
-        run_rover_converter = subprocess.Popen( [post_file, os.path.join(rover_extract, basename(rover_extract)[:-4] + ".obs"),
-        os.path.join(base_extract, basename(base_extract)[:-4] + ".obs"), 
-        os.path.join(base_extract, basename(base_extract)[:-4] + ".nav"),
-        "-o", os.path.join(rover_extract, basename(rover_extract)[:-4] + ".pos"),
-        "-c", "on", 
-        "-h", "on",
-        "-p", mode] )
-   
-    if base_pos_prompt == 1: # Position file is given
-        run_rover_converter = subprocess.Popen( [post_file, os.path.join(rover_extract, basename(rover_extract)[:-4] + ".obs"),
-        os.path.join(base_extract, basename(base_extract)[:-4] + ".obs"), 
-        os.path.join(base_extract, basename(base_extract)[:-4] + ".nav"),
-        "-o", os.path.join(rover_extract, basename(rover_extract)[:-4] + ".pos"),
-        "-l", [base_position],
-        "-c", "on",
-        "-h", "on",
-        "-p", mode] )
+    if base_pos_prompt == 0:  # No position file is given
+        run_rover_converter = subprocess.Popen([post_file, os.path.join(rover_extract, basename(rover_extract)[:-4] + ".obs"),
+                                                os.path.join(base_extract, basename(
+                                                    base_extract)[:-4] + ".obs"),
+                                                os.path.join(base_extract, basename(
+                                                    base_extract)[:-4] + ".nav"),
+                                                "-o", os.path.join(rover_extract,
+                                                                   basename(rover_extract)[:-4] + ".pos"),
+                                                "-c", "on",
+                                                "-h", "on",
+                                                "-p", mode])
 
+    if base_pos_prompt == 1:  # Position file is given
+        run_rover_converter = subprocess.Popen([post_file, os.path.join(rover_extract, basename(rover_extract)[:-4] + ".obs"),
+                                                os.path.join(base_extract, basename(
+                                                    base_extract)[:-4] + ".obs"),
+                                                os.path.join(base_extract, basename(
+                                                    base_extract)[:-4] + ".nav"),
+                                                "-o", os.path.join(rover_extract,
+                                                                   basename(rover_extract)[:-4] + ".pos"),
+                                                "-l", [base_position],
+                                                "-c", "on",
+                                                "-h", "on",
+                                                "-p", mode])
 
     run_rover_converter.wait()
